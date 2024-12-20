13a5d978336774392de7b5964df476e6737b6d7d
sid <duckworth124@github.com>
2024-12-20 19:11:21 +0000

    refactor

--- 2024/day_04/src/main.rs
+++ 2024/day_04/src/main.rs
@@ -1,27 +1,26 @@
-use std::{fs::read_to_string, iter};
-
-use itertools::Itertools;
+use itertools::{iterate, Itertools};
+use std::{char, fs::read_to_string};
 
 #[derive(Clone, Copy)]
 struct Position {
-    x: usize,
-    y: usize,
+    x: i32,
+    y: i32,
 }
 
 impl Position {
-    fn step(self, direction: Direction) -> Option<Self> {
+    fn step(self, direction: Direction) -> Self {
         let (x, y) = match direction {
-            Direction::Up => (self.x, self.y.checked_sub(1)?),
+            Direction::Up => (self.x, self.y - 1),
             Direction::Down => (self.x, self.y + 1),
-            Direction::Left => (self.x.checked_sub(1)?, self.y),
+            Direction::Left => (self.x - 1, self.y),
             Direction::Right => (self.x + 1, self.y),
-            Direction::UpLeft => (self.x.checked_sub(1)?, self.y.checked_sub(1)?),
-            Direction::UpRight => (self.x + 1, self.y.checked_sub(1)?),
-            Direction::DownLeft => (self.x.checked_sub(1)?, self.y + 1),
+            Direction::UpLeft => (self.x - 1, self.y - 1),
+            Direction::UpRight => (self.x + 1, self.y - 1),
+            Direction::DownLeft => (self.x - 1, self.y + 1),
             Direction::DownRight => (self.x + 1, self.y + 1),
         };
 
-        Some(Position { x, y })
+        Position { x, y }
     }
 }
 
@@ -52,18 +51,17 @@ impl Direction {
     }
 }
 
+fn get(grid: &[Vec<char>], position: Position) -> Option<char> {
+    let y: usize = position.y.try_into().ok()?;
+    let x: usize = position.x.try_into().ok()?;
+    grid.get(y)?.get(x).copied()
+}
+
 fn is_xmas(grid: &[Vec<char>], position: Position, direction: Direction) -> bool {
-    let width = grid[0].len();
-    let height = grid.len();
-    let chars = iter::successors(
-        Some(position).filter(|p| p.x < width && p.y < height),
-        |p| p.step(direction).filter(|p| p.x < width && p.y < height),
-    )
-    .take(4)
-    .flat_map(|p| grid.get(p.y).map(|r| r.get(p.x)))
-    .flatten()
-    .copied()
-    .collect_vec();
+    let chars = iterate(position, |p| p.step(direction))
+        .take(4)
+        .flat_map(|p| get(grid, p))
+        .collect_vec();
 
     chars == vec!['X', 'M', 'A', 'S']
 }
@@ -73,6 +71,7 @@ fn count_xmas(grid: &[Vec<char>]) -> usize {
     let height = grid.len();
     (0..width)
         .cartesian_product(0..height)
+        .map(|(x, y)| (x as i32, y as i32))
         .map(|(x, y)| Position { x, y })
         .cartesian_product(Direction::all())
         .filter(|(p, d)| is_xmas(grid, *p, *d))
@@ -80,7 +79,7 @@ fn count_xmas(grid: &[Vec<char>]) -> usize {
 }
 
 fn is_centre_of_x_mas(grid: &[Vec<char>], position: Position) -> bool {
-    grid[position.y][position.x] == 'A'
+    grid[position.y as usize][position.x as usize] == 'A'
         && ["MMSS", "SSMM", "SMMS", "MSSM"].contains(
             &[
                 Direction::UpRight,
@@ -89,10 +88,8 @@ fn is_centre_of_x_mas(grid: &[Vec<char>], position: Position) -> bool {
                 Direction::UpLeft,
             ]
             .iter()
-            .filter_map(|d| position.step(*d))
-            .flat_map(|p| grid.get(p.y).map(|r| r.get(p.x)))
-            .flatten()
-            .copied()
+            .map(|d| position.step(*d))
+            .flat_map(|p| get(grid, p))
             .collect::<String>()
             .as_str(),
         )
@@ -103,6 +100,7 @@ fn count_x_mas(grid: &[Vec<char>]) -> usize {
     let height = grid.len();
     (1..width - 1)
         .cartesian_product(1..height - 1)
+        .map(|(x, y)| (x as i32, y as i32))
         .map(|(x, y)| Position { x, y })
         .filter(|p| is_centre_of_x_mas(grid, *p))
         .count()
@@ -111,9 +109,9 @@ fn count_x_mas(grid: &[Vec<char>]) -> usize {
 fn solve(path: &str) -> (usize, usize) {
     let input = read_to_string(path).unwrap();
     let grid: Vec<_> = input.lines().map(|l| l.chars().collect()).collect();
-    let outptu_1 = count_xmas(&grid);
+    let output_1 = count_xmas(&grid);
     let output_2 = count_x_mas(&grid);
-    (outptu_1, output_2)
+    (output_1, output_2)
 }
 
 fn main() {
