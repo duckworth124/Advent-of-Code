1c7724d823e14d71cd13d0e72c5d6ea620c92f71
sid <duckworth124@github.com>
2024-12-20 18:56:15 +0000

    nom > regex

--- 2024/day_03/src/main.rs
+++ 2024/day_03/src/main.rs
@@ -1,12 +1,25 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{anychar, u32},
+    combinator::value,
+    multi::many0,
+    sequence::tuple,
+    Parser,
+};
 use std::fs::read_to_string;
 
-use regex::Regex;
-
 fn mul_sum(input: &str) -> u32 {
-    let pat = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    pat.captures_iter(input)
-        .map(|c| c[1].parse::<u32>().unwrap() * c[2].parse::<u32>().unwrap())
-        .sum()
+    many0(alt((
+        tuple((tag("mul("), u32::<&str, ()>, tag(","), u32, tag(")")))
+            .map(|(_, x, _, y, _)| x * y)
+            .map(Some),
+        value(None, anychar),
+    )))
+    .map(|v| v.into_iter().flatten().sum())
+    .parse(input)
+    .unwrap()
+    .1
 }
 
 fn mul_sum_cond(input: &str) -> u32 {
