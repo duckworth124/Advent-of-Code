fef318666bed0640301d6f38795a734fb6c5a75e
sid <duckworth124@github.com>
2025-04-02 20:24:54 +0100

    some 2016

--- 2016/day_01/src/main.rs
+++ 2016/day_01/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, fs::read_to_string};
+
 #[derive(Clone, Copy)]
 enum Direction {
     Up,
@@ -30,12 +32,50 @@ impl Direction {
     }
 }
 
-fn solve(instructions: &[(char, u32)]) -> u32 {
-    instructions
+fn solve(instructions: &[(char, usize)]) -> (i32, i32) {
+    let path: Vec<(i32, i32)> = instructions
         .iter()
-        .scan(Direction::Right, |direction, (c, d)| todo!());
+        .scan(Direction::Up, |d, &(c, n)| {
+            *d = match c {
+                'R' => d.rotate_right(),
+                'L' => d.rotate_left(),
+                _ => panic!("unrecognized character : {c}"),
+            };
+            Some((*d, n))
+        })
+        .flat_map(|(d, n)| vec![d; n])
+        .scan((0, 0), |(x, y), d| {
+            let (dx, dy) = d.to_coords();
+            *x += dx;
+            *y += dy;
+            Some((*x, *y))
+        })
+        .collect();
+
+    let (x, y) = path.last().unwrap();
+    let output_1 = x.abs() + y.abs();
+
+    let mut visited = HashSet::new();
+
+    let first_repeat = path.into_iter().find(|&p| !visited.insert(p)).unwrap();
+
+    let (x, y) = first_repeat;
+    let output_2 = x.abs() + y.abs();
+
+    (output_1, output_2)
 }
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_to_string("input").unwrap();
+    let instructions = input
+        .trim()
+        .split(", ")
+        .map(|s| {
+            let (c, n) = s.split_at(1);
+            (c.chars().next().unwrap(), n.parse().unwrap())
+        })
+        .collect::<Vec<_>>();
+
+    let (output_1, output_2) = solve(&instructions);
+    println!("part 1: {output_1} part 2: {output_2}")
 }
