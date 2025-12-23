f4678a88e135e5b78eee53f66b8e989845bf4652
sid <duckworth124@github.com>
2025-12-23 23:20:35 +0000

    changed from u128 to u64 and added saturating methods

--- 2025/day_02/src/main.rs
+++ 2025/day_02/src/main.rs
@@ -1,29 +1,34 @@
 use std::{collections::HashSet, fs::read_to_string, iter, time::Instant};
 
-fn invalid_ids(low: u128, high: u128) -> impl Iterator<Item = u128> {
+fn invalid_ids(low: u64, high: u64) -> impl Iterator<Item = u64> {
     (1..)
-        .map(|x| x * (10_u128.pow(num_digits(x)) + 1))
+        .map(|x| x * (10_u64.pow(num_digits(x)) + 1))
         .skip_while(move |&x| x < low)
         .take_while(move |&x| x <= high)
 }
 
-fn invalid_ids_many_repeats(low: u128, high: u128) -> HashSet<u128> {
+fn invalid_ids_many_repeats(low: u64, high: u64) -> HashSet<u64> {
     (1..)
-        .take_while(|&x| x * 10_u128.pow(num_digits(x)) + x <= high)
+        .take_while(|&x| x * 10_u64.pow(num_digits(x)) + x <= high)
         .flat_map(|x| {
-            iter::successors(Some(x), move |&n| Some(n * 10_u128.pow(num_digits(x)) + x))
-                .skip(1)
-                .skip_while(|&x| x < low)
-                .take_while(|&x| x <= high)
+            iter::successors(Some(x), move |&n| {
+                Some(
+                    n.saturating_mul(10_u64.saturating_pow(num_digits(x)))
+                        .saturating_add(x),
+                )
+            })
+            .skip(1)
+            .skip_while(|&x| x < low)
+            .take_while(|&x| x <= high)
         })
         .collect()
 }
 
-const fn num_digits(x: u128) -> u32 {
+const fn num_digits(x: u64) -> u32 {
     x.ilog10() + 1
 }
 
-fn solve(input: &str) -> (u128, u128) {
+fn solve(input: &str) -> (u64, u64) {
     let mut output_1 = 0;
     let mut output_2 = 0;
     for (low, high) in input
@@ -31,10 +36,8 @@ fn solve(input: &str) -> (u128, u128) {
         .map(|s| s.split_once('-').unwrap())
         .map(|(l, r)| (l.parse().unwrap(), r.parse().unwrap()))
     {
-        output_1 += invalid_ids(low, high).sum::<u128>();
-        output_2 += invalid_ids_many_repeats(low, high)
-            .into_iter()
-            .sum::<u128>();
+        output_1 += invalid_ids(low, high).sum::<u64>();
+        output_2 += invalid_ids_many_repeats(low, high).into_iter().sum::<u64>();
     }
 
     (output_1, output_2)
